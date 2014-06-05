b1595486326f634cbf1a4d87cb5b0d615165e79f
Dorenavant <Ignatius@MaryvaleMBP15.local>
2014-06-05 11:06:43 -0400

    added unfinished buttons

--- site/src/SecretSpies/MainMenuState.js
+++ site/src/SecretSpies/MainMenuState.js
@@ -14,12 +14,21 @@ this.SecretSpies = this.SecretSpies || {};
 
     p.preload = function() {
         var assets = SecretSpies.path.assets;
+        console.log(assets);
         this.load.image("MainMenuState/background", assets.level.child("mainMenu/introscreen.png"));
+        this.load.image("MainMenuState/buttons", assets.common.child("textures/buttons.png"));
+        // this.load.spritesheet("MainMenuState/buttons", assets.common.child("textures/buttons.png"), 558, 64);
+        this.load.image("MainMenuState/world-s-screen"), assets.level.child("mainMenu/world-map-screen.png");
     }
 
     p.create = function() {
         var background = this.objects["background"] = this.add.sprite(0, 0, "MainMenuState/background");
         SecretSpies.resizeSpriteTo(background, this.stage.bounds.width, this.stage.bounds.height);  
+        var button;
+        button = this.add.button(this.world.centerX + 95, 400, "buttons", actionOnClick, this, 2, 1 , 0);
+        function actionOnClick () {
+            background = this.objects["world-map-screen"];
+        }
     }
 
     p.update = function() {
